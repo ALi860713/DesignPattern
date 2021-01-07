98a57ecba9b457fdc8b5c66ee8498f4c8e75bf8e
Ali <w11160212@gmail.com>
2021-01-08 00:17:00 +0800

    add scenario

---
+++ DesignPattern/Scenario/FileService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleFactory.Scenario
+{
+    internal class FileService
+    {
+        public void Upload()
+        {
+            var ftpServer = new FtpProvider();
+            ftpServer.UploadFile();
+        }
+
+        public void Send()
+        {
+            var ftpServer = new FtpProvider();
+            ftpServer.SendFile();
+        }
+    }
+}
---
+++ DesignPattern/Scenario/FtpProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleFactory.Scenario
+{
+    internal class FtpProvider
+    {
+        public void UploadFile()
+        {
+            Console.WriteLine("FtpProvider uploading file...");
+        }
+
+        public void SendFile()
+        {
+            Console.WriteLine("FtpProvider sending file...");
+        }
+    }
+}
