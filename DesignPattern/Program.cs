fba23231e52fe3d9d3224be4c5b8391a0c408058
Ali <w11160212@gmail.com>
2021-01-07 23:24:23 +0800

    <SimpleFactory> rename project and add sample code

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 
-namespace DesignPattern
+namespace SimpleFactory
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var tcpCommunication = CommunicationFactory.GetInstance(CommunicationType.Tcp);
+            tcpCommunication.ConnectToNetwork();
+            var udpCommunication = CommunicationFactory.GetInstance(CommunicationType.Udp);
+            udpCommunication.ConnectToNetwork();
+            Console.ReadLine();
         }
+    }
+
+    public class CommunicationFactory
+    {
+        public static ICommunication GetInstance(CommunicationType communication)
+        {
+            switch (communication)
+            {
+                case CommunicationType.Tcp:
+                    return new TcpCommunication();
+                case CommunicationType.Udp:
+                    return new UdpCommunication();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public class UdpCommunication : ICommunication
+    {
+        public void ConnectToNetwork()
+        {
+            Console.WriteLine("UdpCommunication ConnectToNetwork");
+        }
+    }
+
+    public class TcpCommunication : ICommunication
+    {
+        public void ConnectToNetwork()
+        {
+            Console.WriteLine("TcpCommunication ConnectToNetwork");
+        }
+    }
+
+    public enum CommunicationType
+    {
+        Tcp,
+        Udp
+    }
+
+    public interface ICommunication
+    {
+        void ConnectToNetwork();
     }
 }
