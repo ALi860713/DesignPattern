6ecc1690ba3f26038c717f46bb32ac2762c2a2f4
Ali <w11160212@gmail.com>
2021-01-08 02:35:09 +0800

    <Facade> add sample code

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -6,7 +6,61 @@ namespace Facade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var facade = new Computer();
+            facade.StartComputer();
+        }
+    }
+
+    internal class Computer
+    {
+        private readonly Cpu _cpu;
+        private readonly SystemMemory _memory;
+        private readonly Bios _bios;
+
+        public Computer()
+        {
+            _cpu = new Cpu();
+            _memory = new SystemMemory();
+            _bios = new Bios();
+        }
+
+        private const string BootAddress = "/boot/bin";
+
+        public void StartComputer()
+        {
+            _cpu.Freeze();
+            _memory.Load(BootAddress);
+            _bios.InitialInOutput();
+            _cpu.Execute();
+        }
+    }
+
+    internal class SystemMemory
+    {
+        public void Load(string bootAddress)
+        {
+            Console.WriteLine($"System memory loading {bootAddress}...");
+        }
+    }
+
+    internal class Cpu
+    {
+        public void Freeze()
+        {
+            Console.WriteLine("Cpu freezing...");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Cpu executing...");
+        }
+    }
+
+    internal class Bios
+    {
+        public void InitialInOutput()
+        {
+            Console.WriteLine("Bios initialing input and output system...");
         }
     }
 }
