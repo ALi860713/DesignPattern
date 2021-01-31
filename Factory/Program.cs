c2abf353bdb0b4950b0a1acfd66058ab8e8f5362
Ali <w11160212@gmail.com>
2021-01-31 19:59:00 +0800

    <Factory> add project and sample code

---
+++ Factory/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var storeACommunication = new StoreACommunicationFactory().CreateCommunication();
+            storeACommunication.ConnectToNetwork();
+            var storeBCommunication = new StoreBCommunicationFactory().CreateCommunication();
+            storeBCommunication.ConnectToNetwork();
+        }
+    }
+
+    public interface ICommunicationFactory
+    {
+        ICommunication CreateCommunication();
+    }
+
+    public class StoreACommunicationFactory : ICommunicationFactory
+    {
+        public ICommunication CreateCommunication()
+        {
+            return new TcpCommunication();
+        }
+    }
+    public class StoreBCommunicationFactory : ICommunicationFactory
+    {
+        public ICommunication CreateCommunication()
+        {
+            return new UdpCommunication();
+        }
+    }
+
+    public interface ICommunication
+    {
+        void ConnectToNetwork();
+        void Disconnect();
+    }
+
+    public class UdpCommunication : ICommunication
+    {
+        public void ConnectToNetwork()
+        {
+            Console.WriteLine("UdpCommunication ConnectToNetwork");
+        }
+
+        public void Disconnect()
+        {
+            Console.WriteLine("UdpCommunication Disconnect");
+        }
+    }
+
+    public class TcpCommunication : ICommunication
+    {
+        public void ConnectToNetwork()
+        {
+            Console.WriteLine("TcpCommunication ConnectToNetwork");
+        }
+
+        public void Disconnect()
+        {
+            Console.WriteLine("TcpCommunication Disconnect");
+        }
+    }
+}
